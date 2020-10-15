6060eeb9375b2aa9eaee62f1ca95698221dbe040
Brychan Dempsey <brychand@hotmail.com>
2020-10-15 23:03:13 +1300

    Retargeted for .NET 4.5 Added application information

--- Admin/Properties/AssemblyInfo.cs
+++ Admin/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Admin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Launches a new PowerShell window with Administrator privelleges")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Admin")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © Brychan Dempsey 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
